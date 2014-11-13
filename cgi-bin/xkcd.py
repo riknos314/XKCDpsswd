da1324acdd0a92338f369e2ea6f1e7905324950b
riknos314 <riknos314@gmail.com>
2014-11-13 00:34:44 -0600

    moarstuffs

--- cgi-bin/xkcd.py
+++ cgi-bin/xkcd.py
@@ -1,12 +1,32 @@
 #!/usr/bin/python
 import os
-
-dictionary = [i.strip() for i in open('/usr/share/dict/american-english').readlines()]
-
-#stringofnames = os.environ['QUERY_STRING']
+import random
 
 
+def main():
+	dictionary = [i.strip() for i in open('/usr/share/dict/american-english').readlines()]
+	wordone = dictionary[ random.randrange(0,len(dictionary)-1,1)]
+	wordtwo = dictionary[ random.randrange(0,len(dictionary)-1,1)]
+	wordthree = dictionary[ random.randrange(0,len(dictionary)-1,1)]
+	wordfour = dictionary[ random.randrange(0,len(dictionary)-1,1)]
+
+
+	stringofnames = os.environ['QUERY_STRING']
+	namepairs = stringofnames.split('=')
+
+	astring'''
+	<!DOCTYPE html>
+	<html>
+
+		<body>
+			<p> {0}</p>
+		</body>
+	</html>
+
+	'''.format(namepairs[0])
+	print(astring)
 
 
 
-#namepairs = stringofnames.split('=')
+if __name__ == (__main__):
+	main()
